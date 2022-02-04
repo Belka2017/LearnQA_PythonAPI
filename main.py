9c4815b3b0c26ceb3458b3b4510632425fb4c376
Ella Gololobova <villo4k@mail.ru>
2022-02-04 09:53:37 +0300

    I am learning to parse JSON

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+from json.decoder import JSONDecodeError
 import requests
 
+payload = {"name": "User"}
 responce = requests.get("https://playground.learnqa.ru/api/hello")
 print(responce.text)
+
+try
+    parsed_responce_txt = responce.json()
+    print(parsed_responce_txt)
+except JSONDecodeError:
+    print("Response is not a JSON")
