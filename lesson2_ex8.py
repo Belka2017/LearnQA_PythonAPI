2d50b04caead41d54707f61200350511726872dd
Ella Gololobova <villo4k@mail.ru>
2022-02-10 15:24:36 +0300

    Added assert for task 8 lesson2

--- lesson2_ex8.py
+++ lesson2_ex8.py
@@ -9,10 +9,17 @@ t = obj['seconds']
 resp2 = requests.get('https://playground.learnqa.ru/ajax/api/longtime_job', params=token)
 obj2 = resp2.json()
 status = obj2['status']
-print(f'Status before {t} sec: {status}')
+if status == 'Job is NOT ready':
+    print(f'Status before {t} sec: {status}')
+else:
+    print(f'Incorrect status for task before {t} sec')
 
 time.sleep(t)
 resp3 = requests.get('https://playground.learnqa.ru/ajax/api/longtime_job', params=token)
 obj3 = resp3.json()
-print(f"Status after {t} sec : {obj3['status']}")
+status = obj3['status']
+if status == 'Job is ready':
+    print(f"Status after {t} sec : {status}")
+else:
+    print(f'Incorrect status for task after {t} sec')
 print(f"Result: {obj3['result']}")
