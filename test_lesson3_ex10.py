cde8f869b490c797490beac6276fffda40059bb5
Ella Gololobova <villo4k@mail.ru>
2022-02-13 20:44:24 +0300

    Fix name of class

--- test_lesson3_ex10.py
+++ test_lesson3_ex10.py
@@ -1,5 +1,5 @@
-class Phrase:
+class TestPhraseInput:
     def test_phrase_lenght(self):
         phrase = input("Set a phrase: ")
         max = 15
-        assert len(phrase) >= max, f"The length of the phrase is {len(phrase)}. It's more then {max}"
+        assert len(phrase) <= max, f"The length of the phrase is {len(phrase)}. It's more then {max}"
